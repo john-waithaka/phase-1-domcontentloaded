36d30f36504781637d224b591f9b4aa6562801cc
John Waithaka <johnwaruiwaithaka@gmail.com>
2024-04-13 13:15:34 +0300

    innerHtml property

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+//add an event listener and a call back fxn
+  document.addEventListener("DOMContentLoaded", function() {
+    //calls up the identified "text" in the HTML
+    innerHtml = document.getElementById("text");
+    //calls up what's written in the text element and changes it to what you want
+    innerHtml.innerHTML = "This is really cool!";
+    //not very necessary but for purposes of control
+    console.log("This is really cool!" );
+  });
+
+
+//or this coz of scope and it can be used
+
+//add an event listener
+  document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  //update the message after the DOM fully loads
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
